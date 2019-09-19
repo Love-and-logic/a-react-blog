384330cdba34106924f1b2aae24ed22c46d62c92
love-and-logic <ejatbcc@gmail.com>
2019-09-19 02:03:33 -0400

    initial commit

---
+++ src/build/precache-manifest.f3396204b7fb636cc6fb90d9c3754964.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6721a408cbe045af7e8df5b8d8900c4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "add4a1556285b17809a4",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "67186f0e5c35931694a7",
+    "url": "/static/js/2.94fba53b.chunk.js"
+  },
+  {
+    "revision": "add4a1556285b17809a4",
+    "url": "/static/js/main.2f4e138f.chunk.js"
+  },
+  {
+    "revision": "16f57bc6dde3f2278832",
+    "url": "/static/js/runtime~main.82f07dd6.js"
+  }
+]);
